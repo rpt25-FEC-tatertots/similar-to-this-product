eb69dd094cf6c9a7ceb46d863121c274614c0cae
Mason Pott <masoncpott@gmail.com>
2021-01-29 11:23:33 -0600

    confirmed mongoose only saves unique product numbers to the database, does not save duplicates

---
+++ database/dataGeneration.js
@@ -0,0 +1,3 @@
+// generate fake data to add to my database
+
+// when this file is ran, add all the data to the database
--- database/schema.js
+++ database/schema.js
@@ -1,34 +1,37 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/similarToThisProduct', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb://localhost:27017/similarToThisProduct', {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
 
 const db = mongoose.connection;
 
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
+db
+  .on('error', console.error.bind(console, 'connection error:'))
+  .once('open', function() {
   console.log('CONNECTION TO DATABASE SUCCESSFUL');
-
-  const productSchema = new mongoose.Schema({
-    productNumber: {
-      type: Number,
-      min: 0,
-      max: 101
-    },
-    associatedProductNumbers: [Number]
   });
 
-  const Product = mongoose.model('Product', productSchema);
+const productSchema = new mongoose.Schema({
+  productNumber: {
+    type: Number,
+    unique: true,
+    index: true,
+    min: 0,
+    max: 101
+  },
+  associatedProductNumbers: [Number]
+});
 
-  const firstTestProduct = new Product({
-    productNumber: 1,
-    associatedProductNumbers: [2, 3, 4, 5, 6]
-  })
+const Product = mongoose.model('Product', productSchema);
 
-  firstTestProduct.save(function(err) {
-    if (err) console.log('ERROR SAVING INTO DB');
-  })
+const firstTestProduct = new Product({
+  productNumber: 1,
+  associatedProductNumbers: [2, 3, 4, 5, 6]
+})
 
-  Product.find(function (err, products) {
-    if (err) console.log('ERROR RETURNING FROM DB');
-    console.log('RETRIEVED FROM DB: ', products);
-  });
+firstTestProduct.save(function(err) {
+  if (err) console.log('ERROR SAVING INTO DB');
+})
+
+Product.find(function (err, products) {
+  if (err) console.log('ERROR RETURNING FROM DB');
+  console.log('RETRIEVED FROM DB: ', products);
 });
