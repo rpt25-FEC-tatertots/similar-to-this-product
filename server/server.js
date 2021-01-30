0ffc252340991a0220f8e9e5046891f398308f99
Mason Pott <masoncpott@gmail.com>
2021-01-29 22:01:47 -0600

    made some notes about routes/get requests i need to make tomorrow

--- server/server.js
+++ server/server.js
@@ -8,8 +8,17 @@ app.use(express.urlencoded());
 app.use(express.json());
 
 app.get('/similar', (req, res) => {
+  // needs to get the product numbers (array) related to the current product number
+  //for each productNumber in the array, it needs to collect all the necessary data from the other apis
+  //needs to then send all this information back to be rendered
   res.send(200)
 })
+
+// need to add routes for the following:
+
+// title API to get color info, pricing, and product names
+// overviewAPI to get icon information
+// photo API to get photos for eact product
 
 app.listen(6008, function () {
   console.log('listening on port 3000!');
