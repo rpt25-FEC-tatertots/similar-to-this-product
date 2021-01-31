dee9fcab122b71dc869213859e05488cb24fe28e
Mason Pott <masoncpott@gmail.com>
2021-01-31 14:36:23 -0600

    build out mock data, sending it to front end so i can build the actual component

--- server/server.js
+++ server/server.js
@@ -14,49 +14,33 @@ app.get('/similar', async (req, res) => {
   const { product_id } = req.query;
   try {
     const associatedProductNumbers = await db.getAssociatedProductNums(product_id)
-  //for each productNumber in the array, it needs to collect all the necessary data from the other apis
-  //   // [2, 34, 56, 87]
-    const allProductData = associatedProductNumbers.map(async (productNumber) => {
-      console.log('WORKING INSIDE THE MAP');
-        try {
-          const titleInfo = await axios.get(`/title/?product_id=${productNumber}`)
-          const inventoryInfo = await axios.get(`/inventory/?product_id=${productNumber}`)
-          const iconInfo = await axios.get(`overview/icons/?product_id=${productNumber}`)
-          const imagesInfo = await axios.get(`/images/mainImages/?product_id=${productNumber}`)
-        } catch (err) {
-          console.log('ERROR INSIDE CATCH BLOCK OF SERVER DISTRIBUTION: ', err)
-        }
-      })
-      Promise.all(allProductData).then(response => {
-        console.log('RESPONSE', response)
-      })
     } catch (error) {
       console.log('ERROR IN SERVER: ', error)
     }
-  //needs to then send all this information back to be rendered
-  res.send(200)
+  // FOR DEV PURPOSES WE ARE JUST SENDING MOCK DATA SO I CAN BUILD OUT A FRONT END THAT WORKS
+  res.send(mockData)
 })
 
-app.get('/title/', (req, res) => {
-  // res.send(mockData.mockTitleData)
-  console.log('HIT TITLE API')
-  res.send('SUCCESS IN TITLE API')
-})
+// app.get('/title/', (req, res) => {
+//   // res.send(mockData.mockTitleData)
+//   console.log('HIT TITLE API')
+//   res.send('SUCCESS IN TITLE API')
+// })
 
-app.get('/inventory/', (req, res) => {
-    // res.send(mockData.mockInventoryData)
-    res.send(200)
-})
+// app.get('/inventory/', (req, res) => {
+//     // res.send(mockData.mockInventoryData)
+//     res.send(200)
+// })
 
-app.get('overview/icons/', (req, res) => {
-  // res.send(mockData.mockIconData)
-  res.send(200)
-})
+// app.get('overview/icons/', (req, res) => {
+//   // res.send(mockData.mockIconData)
+//   res.send(200)
+// })
 
-app.get('/images/mainImages/', (req, res) => {
-    // res.send(mockData.mockImageData)
-    res.send(200)
-})
+// app.get('/images/mainImages/', (req, res) => {
+//     // res.send(mockData.mockImageData)
+//     res.send(200)
+// })
 
 app.listen(6008, function () {
   console.log('listening on port 3000!');
